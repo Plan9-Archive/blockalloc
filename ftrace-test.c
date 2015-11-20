436f8de777d099cfd68f84e700a0432e384c7767
Matthew Veety <mveety@gmail.com>
2015-11-20 00:26:23 -0500

    added more shit to ftrace-lite, fixed fuckups, imported ftrace-test

---
+++ ftrace-test.c
@@ -0,0 +1,58 @@
+#include <u.h>
+#include <libc.h>
+#include <blockalloc.h>
+
+void fn1(void);
+void fn2(void);
+
+QLock g;
+
+void
+main(int argc, char *argv[])
+{
+	initinstru();
+	QLock l;
+	Fn *f = infn("main");
+	action(f,"test1");
+	fn1();
+	action(f,"test2");
+	fn2();
+	outfn(f);
+	Fn *f2 = infn("main 2");
+	shit(f2,"this is a not so bad error");
+	iqlock(f2,&l);
+	iqunlock(f2,&l);
+	upanic(f2,"real bad error");
+	outfn(f2);
+	return;
+}
+
+void
+fn1(void)
+{
+	Fn *f = infn("fn1");
+	iqlock(f,&g);
+	if(rfork(RFPROC)==0){
+		iqunlock(f,&g);
+		upanic(f,"penis");
+	}
+	outfn(f);
+}
+
+void
+fn2(void)
+{
+	Fn *f = infn("fn2");
+	if((int x = rfork(RFPROC)) == 0){
+		Fn *f2 = infn("fn2@proc");
+		fn1();
+		outfn(f2);
+	} else if(x == -1){
+		char *s = mallocz(sizeof(char)*1024,1);
+		errstr(s,1024);
+		upanic(f,smprint("actual error. could not rfork: errstr = %s",serr));
+	} else {
+		action(f,smprint("started process pid = %d",x));
+	}
+	outfn(f);
+}
